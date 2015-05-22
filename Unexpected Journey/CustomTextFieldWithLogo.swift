92e5c151509ef9931757f13284ffa604130d7d9f
Johnny <jszeto0724@gmail.com>
2015-05-21 21:06:36 -0400

    New sign in screen, added email and password text input.

---
+++ Unexpected Journey/CustomTextFieldWithLogo.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTextField.swift
+//  Unexpected Journey
+//
+//  Created by Jun Situ on 5/21/15.
+//  Copyright (c) 2015 Tech Steam. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextFieldWithLogo: UITextField {
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        var borderColor : UIColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0)
+        self.layer.borderColor = borderColor.CGColor
+        self.layer.borderWidth = 1
+    }
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 50.0, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return self.textRectForBounds(bounds)
+    }
+    
+}
---
+++ Unexpected Journey/SignInViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SignInViewController.swift
+//  Unexpected Journey
+//
+//  Created by Jun Situ on 5/21/15.
+//  Copyright (c) 2015 Tech Steam. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var EmailTextInput: CustomTextFieldWithLogo!
+    @IBOutlet weak var PasswordTextInput: CustomTextFieldWithLogo!
+    @IBOutlet weak var UserIcon: UIImageView!
+    @IBOutlet weak var PasswordIcon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        EmailTextInput.delegate = self
+        PasswordTextInput.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.tag == 1 {
+            EmailTextInput.layer.borderColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.0).CGColor
+            UserIcon.image = UIImage(named: "user-active")
+        }
+        
+        if textField.tag == 2 {
+            PasswordTextInput.layer.borderColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.0).CGColor
+            PasswordIcon.image = UIImage(named: "password-active")
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField.tag == 1 {
+            EmailTextInput.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0).CGColor
+            UserIcon.image = UIImage(named: "user")
+        }
+        
+        if textField.tag == 2 {
+            PasswordTextInput.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0).CGColor
+            PasswordIcon.image = UIImage(named: "password")
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
